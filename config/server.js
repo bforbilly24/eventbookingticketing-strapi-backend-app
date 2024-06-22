c2afb46e63840cf4c9012f5073a707df60532dde
bforbilly24 <halimputra0701@gmail.com>
2024-06-23 04:16:48 +0700

    refactor(config)P rollback remove config url

--- config/server.js
+++ config/server.js
@@ -1,15 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env(
-    "PUBLIC_URL",
-    "https://eventbookingticketing-backend.bforbilly.tech"
-  ),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS'),
   },
-  proxy: true,
   webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
