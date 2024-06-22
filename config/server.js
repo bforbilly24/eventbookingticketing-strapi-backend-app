c2f6d2783471a2284f7d21d1eaa9928413e97838
bforbilly24 <halimputra0701@gmail.com>
2024-06-23 03:52:50 +0700

    refactor(config): update config rnv url

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env(
+    "PUBLIC_URL",
+    "https://eventbookingticketing-backend.bforbilly.tech"
+  ),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+  proxy: true,
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
